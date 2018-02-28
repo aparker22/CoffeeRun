0471f1c9023791aea93fad9a4bb1f2ab8ea26f76
Ashley Parker <aparker7698@att.net>
2018-02-28 11:18:55 -0500

    Basic Coffee Run Functionality

---
+++ scripts/main.js
@@ -0,0 +1,10 @@
+var coffeeOrders = [];
+var form = document.querySelector('body > section > div > div > form');
+
+form.addEventListener('submit', function(event) {
+    event.preventDefault();
+    coffeeOrders.push({'Coffee': form.coffee.value, 'Email': form.emailAddress.value,
+    'Size': form.size.value, 'Flavor': form.flavor.value, 'Strength': form.strength.value
+    });
+    form.reset();
+})
